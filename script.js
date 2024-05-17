84dc9b63610060f3650dbfe448a0b264a26d0604
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-05-17 19:35:13 +0200

    Create modal

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ function Book() {
 function addBookToLibrary() {
   // do stuff here
 }
+
+// MODAL
+const modal = document.querySelector('dialog');
+const openModal = document.querySelector('#add-book');
+const closeModal = document.querySelector('#close-modal'); 
+
+// "Show the dialog" button opens the dialog modally
+openModal.addEventListener("click", () => {
+    modal.showModal();
+});
+  
+  // "Close" button closes the dialog
+closeModal.addEventListener("click", () => {
+    modal.close();
+});
